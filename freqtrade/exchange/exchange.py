aabca903e2d7add0b52e97b9bfc89d59e8956394
flopius <flopodude@gmail.com>
2025-04-03 12:15:20 +0300

    imagine the retardation of not putting it here of mine

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -1733,7 +1733,7 @@ class Exchange:
             symbols = []
             if pair:
                 symbols.append(pair)
-            positions: list[CcxtPosition] = self._api.fetch_positions(symbols)
+            positions: list[CcxtPosition] = self._api.fetch_positions(symbols, params={'limit': 200})
             for position in positions:
                 print(position["symbol"], "official output")
             self._log_exchange_response("fetch_positions", positions)
